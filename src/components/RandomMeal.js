5bfb6b14838124793976de6a99fced3d601f256e
shawn <shawncaceres10@gmail.com>
2021-10-31 08:01:28 -0500

    working api

--- src/components/RandomMeal.js
+++ src/components/RandomMeal.js
@@ -1,12 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const API_URL = 'www.themealdb.com/api/json/v1/1/random.php';
+const API_URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 const RandomMeal = () => {
     const [meal, setMeal]=useState(undefined)
 
+    const getMealRequest = async()=>{
+        try{
+        const response = await fetch(API_URL)
+        const data = await response.json()
+        console.log(data)
+        console.log(data.meals[0].strMeal)
+        setMeal(data.meals[0])
+        }catch(error){
+            console.log(error)
+        }
+
+    
+    }
+    useEffect(()=>{
+        getMealRequest()
+    },[])
+
     return (  
-        <div>Random Meal</div>
+        <> 
+         <div>Random Meal</div>
+         <h1>meal.strMeal</h1>
+        </>
+        
     );
 }
  
